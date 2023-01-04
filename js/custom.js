39c3862c1c85e4cade02c47f3a3c5226a9ab771b
kamilstalinski <kamstal95@gmail.com>
2023-01-04 14:42:45 +0100

    popup added

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,12 @@
 const hamburger = document.querySelector(".nav__hamburger");
 const nav = document.querySelector(".nav");
 const bg = document.querySelector(".bg__blur");
+const playBtn = document.querySelector(".hero__button");
+const popUp = document.getElementById("popup");
+const closeBtn = document.querySelector(".close-btn");
 
 export default function () {
+  //handling opening and closing mobile menu navigation
   hamburger.addEventListener("click", (e) => {
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
@@ -14,5 +18,13 @@ export default function () {
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
     bg.classList.toggle("active");
+  });
+
+  playBtn.addEventListener("click", () => {
+    popUp.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    popUp.classList.remove("active");
   });
 }
