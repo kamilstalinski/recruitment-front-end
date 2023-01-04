4b518c5b4c0abc4293a8516cc27ae8bd33a280cc
kamilstalinski <kamstal95@gmail.com>
2023-01-04 23:19:15 +0100

    rwd for blog section finished

--- js/custom.js
+++ js/custom.js
@@ -6,12 +6,19 @@ const playBtn = document.querySelector(".hero__button");
 const popUp = document.getElementById("popup");
 const closeBtn = document.querySelector(".close-btn");
 
+const b = document.body;
+const html = document.documentElement;
+
 export default function () {
   //handling opening and closing mobile menu navigation
   hamburger.addEventListener("click", (e) => {
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
     bg.classList.toggle("active");
+
+    html.style.overflowY =
+      html.style.overflowY === "hidden" ? "visible" : "hidden";
+    b.style.overflowY = b.style.overflowY === "hidden" ? "visible" : "hidden";
   });
 
   bg.addEventListener("click", () => {
@@ -20,11 +27,20 @@ export default function () {
     bg.classList.toggle("active");
   });
 
+  //handling opening and closing popup
   playBtn.addEventListener("click", () => {
     popUp.classList.add("active");
+
+    html.style.overflowY =
+      html.style.overflowY === "hidden" ? "visible" : "hidden";
+    b.style.overflowY = b.style.overflowY === "hidden" ? "visible" : "hidden";
   });
 
   closeBtn.addEventListener("click", () => {
     popUp.classList.remove("active");
+
+    html.style.overflowY =
+      html.style.overflowY === "hidden" ? "visible" : "hidden";
+    b.style.overflowY = b.style.overflowY === "hidden" ? "visible" : "hidden";
   });
 }
